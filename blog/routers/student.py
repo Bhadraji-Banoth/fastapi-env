fd4cc60d92e85e92cb298701c2798c82c2d1d221
Bhadraji Banoth <bhadraji.banoth@focalpointk12.com>
2025-02-03 18:45:34 +0530

    FastAPI Final stage

---
+++ blog/routers/student.py
@@ -0,0 +1,16 @@
+from typing import List
+from fastapi import APIRouter,Depends
+from .. import schemas, database, models
+from sqlalchemy.orm import Session
+
+router=APIRouter()
+
+get_db=database.get_db
+
+@router.post('/student', tags=["Students"])
+def create(request:schemas.Student,db:Session=Depends(get_db)):
+    new_student=models.Student(name=request.name, gender=request.gender, marks=request.marks)
+    db.add(new_student)
+    db.commit()
+    db.refresh(new_student)
+    return new_student
